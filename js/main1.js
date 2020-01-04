5c4fa9d47cb6900005806888f660c5250dc0b37f
aswin,sekar <aswin.sekar@zohocorp.com>
2020-01-04 16:41:21 +0530

    video changed

--- js/main1.js
+++ js/main1.js
@@ -18,7 +18,7 @@
 
 var forMobile = function () {
 	if ($(window).height() > $(window).width()) {
-		var mobile = "{videoURL:'https://youtu.be/RYOYcl98BpE',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:0, opacity:1, quality:'default',optimizeDisplay:true}"
+		var mobile = "{videoURL:'https://youtu.be/knmVTcPZOmo',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:0, opacity:1, quality:'default',optimizeDisplay:true}"
 		$('.js-home-video a.player').attr('data-property',mobile);
 	} else {
 		var desk = "{videoURL:'https://youtu.be/lNwHNpA4Caw',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:3, opacity:1, quality:'default',optimizeDisplay:true}"
