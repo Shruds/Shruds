a274a7a9c00c303b6d90a754e88b20a31a8afc8b
aswin,sekar <aswin.sekar@zohocorp.com>
2020-01-07 09:31:51 +0530

    video link changed

--- js/main2.js
+++ js/main2.js
@@ -21,7 +21,7 @@ var forMobile = function () {
 		var mobile = "{videoURL:'https://youtu.be/T5OMtl2FEkw',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:0, opacity:1, quality:'highres',optimizeDisplay:true}"
 		$('.js-home-video a.player').attr('data-property',mobile);
 	} else {
-		var desk = "{videoURL:'https://youtu.be/lNwHNpA4Caw',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:3, opacity:1, quality:'highres',optimizeDisplay:true}"
+		var desk = "{videoURL:'https://youtu.be/TEQMFDyMtH8',containment:'#home', showControls:false, autoPlay:true, loop:true, mute:true, startAt:6, opacity:1, quality:'highres',optimizeDisplay:true}"
 		$('.js-home-video a.player').attr('data-property',desk);
 	}
 };
